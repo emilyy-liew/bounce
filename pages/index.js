abcad553f2a4db58414bc50cfe7f9934ccfbe2b9
Brian <brian205@mit.edu>
2023-06-19 10:34:44 -0400

    Changed indent from 4 to 2 spaces. Added components folder for the task and toDoList components.

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,27 @@
 import { useState } from 'react';
 
 function Header({ title }) {
-return <h1>{title ? title : "Default title"}</h1>
+  return <h1>{title ? title : "Default title"}</h1>
 }
 
 export default function HomePage() {
-const names = ["Ada Lovelace", "Grace Hopper", "Margaret Hamilton"]
+  const names = ["Ada Lovelace", "Grace Hopper", "Margaret Hamilton"]
+  const [likes, setLikes] = useState(0)
 
-const [likes, setLikes] = useState(0)
-
-function handleClick() {
+  function handleClick() {
     setLikes(likes + 1)
-}
+  }
 
-return (
+  return (
     <div>
     <Header title="Develop. Preview. Ship. 🚀" />
     <ul>
-        {names.map((name) => (
+      {names.map((name) => (
         <li key={name}>{name}</li>
-        ))}
+      ))}
     </ul>
 
     <button onClick={handleClick}>Like ({likes})</button>
     </div>
-)
+  )
 }
