ff7c94d99b19d4458f57ed20ef9f2be773e30841
emilyy-liew <emilyliew2004@gmail.com>
2023-07-11 18:06:46 -0400

    Fix bugs

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "73a15d24a548f9f8"; }
+/******/ 		__webpack_require__.h = function() { return "a8797016555e79a6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
